5cc88109eec87b7f42996e55d6102ec5bd81b17c
Kevin <kevin@powermovesdev.com>
2019-04-13 23:55:04 -0700

    implement basic ui

--- app/src/main/java/com/pmd/droidexihibition/popularreposapp/ui/MainActivity.kt
+++ app/src/main/java/com/pmd/droidexihibition/popularreposapp/ui/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.pmd.droidexihibition.popularreposapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.pmd.droidexihibition.popularreposapp.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController.navigate(R.id.repoSearchView)
     }
 }
--- app/src/main/java/com/pmd/droidexihibition/popularreposapp/ui/RepoSearchView.kt
+++ app/src/main/java/com/pmd/droidexihibition/popularreposapp/ui/RepoSearchView.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.pmd.droidexihibition.popularreposapp.R
 
 class RepoSearchView : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.repo_search_view, container, false)
     }
 
 }
