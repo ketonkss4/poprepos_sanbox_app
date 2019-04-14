d2f1ce2d706bf5f50c573c0047bf17ebd05edca6
Kevin <kevin@powermovesdev.com>
2019-04-13 22:05:47 -0700

    add gradle dependencies and ui classes

---
+++ app/src/main/java/com/pmd/droidexihibition/popularreposapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pmd.droidexihibition.popularreposapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pmd.droidexihibition.popularreposapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/pmd/droidexihibition/popularreposapp/ui/RepoSearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.pmd.droidexihibition.popularreposapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class RepoSearchViewModel : ViewModel(){
+
+}
